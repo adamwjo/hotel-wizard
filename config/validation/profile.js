9cbd4a77013849dd63196f5cd8a2fbea90bada40
adamwjo <adamwjo@gmail.com>
2019-03-18 13:28:47 -0400

    added react for front-end client

---
+++ config/validation/profile.js
@@ -0,0 +1,10 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty.js');
+
+module.exports = function validateProfileInput(data) {
+    let errors = {};
+    //check handle
+    //check bio
+    //check languages
+}
+
